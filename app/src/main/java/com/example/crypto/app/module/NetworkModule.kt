e8bb4fd7d20c87dc77d8affaf1bc9dbe814a6276
Chelyadinova Nadezhda <>
2022-05-13 12:53:54 +0600

    network module

--- app/src/main/java/com/example/crypto/app/module/NetworkModule.kt
+++ app/src/main/java/com/example/crypto/app/module/NetworkModule.kt
@@ -1,4 +1,20 @@
 package com.example.crypto.app.module
 
-class NetworkModule {
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import java.util.concurrent.TimeUnit
+
+val networkModule = module {
+    single(named("BooksOkhttpClient")) {
+        val logsInterceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        OkHttpClient.Builder()
+            .addInterceptor(logsInterceptor)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
 }
