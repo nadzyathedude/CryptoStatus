f33e72b2732074ceafba1f909da1bf6287c05324
Chelyadinova Nadezhda <>
2022-06-21 13:49:12 +0600

    added currency symbols to recycler

--- app/src/main/java/com/example/crypto/ui/adapter/CurrencyListAdapter.kt
+++ app/src/main/java/com/example/crypto/ui/adapter/CurrencyListAdapter.kt
@@ -5,8 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.crypto.databinding.ListItemBinding
 
+const val RUB_SYMBOL = "\t₽"
+const val EUR_SYMBOL = "\t€"
+const val USD_SYMBOL = "\t$"
+
 class CurrencyListAdapter(private val moneyAmount: List<Float>) :
     RecyclerView.Adapter<CurrencyListVH>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyListVH {
         val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CurrencyListVH(binding)
@@ -14,7 +19,20 @@ class CurrencyListAdapter(private val moneyAmount: List<Float>) :
 
     override fun onBindViewHolder(holder: CurrencyListVH, position: Int) {
         holder.bind(
-            money = moneyAmount[position],
+            //money = moneyAmount[position],
+            money = when (position) {
+                0 -> {
+                    moneyAmount[0].toString() + RUB_SYMBOL
+                }
+                1 -> {
+                    moneyAmount[1].toString() + EUR_SYMBOL
+
+                }
+                else -> {
+                    moneyAmount[2].toString() + USD_SYMBOL
+                }
+            },
+
             name = when (position) {
                 0 -> {
                     "Рубли"
@@ -35,7 +53,7 @@ class CurrencyListAdapter(private val moneyAmount: List<Float>) :
 
 class CurrencyListVH(private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(name: String, money: Float) {
+    fun bind(name: String, money: String) {
         with(binding) {
             moneyAmount.text = money.toString()
             currencyName.text = name
